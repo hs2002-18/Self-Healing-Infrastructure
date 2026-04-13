556f522d0f3ab493dd8c760fb6fd0b2785ec8047
Harsh Shrimali <harshshrimali319@gmail.com>
2026-04-13 07:07:05 +0000

    Fixed Setup File

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum update -y
-yum install docker -y
-service docker start
-usermode -aG docker ec2-user
-curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+apt update -y
+apt install docker.io -y
+systemctl start docker
+systemctl enable docker
+
+usermod -aG docker ubuntu
